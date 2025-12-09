1732b523efebde0294173150b56c73c620a4e2c7
Ha Quang Linh <linhhqth08598@email.com>
2025-12-09 20:19:24 +0700

    Giu lai dung 1 file BUFFETDEMAU1

--- Khanhdog.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	int main = 16;
-	printf("ban bao nhieu tuoi");
-	printf("xin moi nhap tuoi");
-	scanf()
-}
--- nghich3.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	printf("Doan voi nhom mau A\n");
-	printf("A00\n");
-	return 0;
-}
